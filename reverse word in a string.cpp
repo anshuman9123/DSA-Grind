9e5f2656642d88dc83f338fe1274233780818c2b
Anshuman Kumar <Anshuman.oist@gmail.com>
2026-04-19 03:18:13 +0530

    solved reversing of a word

---
+++ reverse word in a string.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    string reverseWords(string s) {
+      string ans="";
+      int n=s.size();
+
+      reverse(s.begin(),s.end());
+      for(int i=0;i<n;i++){
+        string word="";
+
+    while(i<n && s[i]!=' '){
+        word += s[i];
+        i++;
+    }
+
+    reverse(word.begin(),word.end());
+
+    if (word.length() > 0)
+    ans +=" "+word;
+
+      }  
+
+      return ans.substr(1);
+    }
+};
